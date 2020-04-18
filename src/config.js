03e4a8666395fa79f77456d2a5ed81a9ebb00db1
Allan Defrenne <allan.defrenne@grow-your-business.fr>
2020-04-18 11:14:55 +0200

    init contentful client

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    contentful : {
+        space: 'bxrw1pm5eoqx',
+        accessToken: 'lZBN7Vp_hi96d5IU0XXI1SQm0gglKTwfGR_unIMubbI'
+    }
+}
---
+++ src/contentful/contentfulClient.js
@@ -0,0 +1,18 @@
+import config from "./config"
+let contentful = require("contentful");
+const client = contentful.createClient(config.contentful);
+
+export default{
+    getCategories: function (){
+        return client.getEntries({
+            'content_type': 'categorie'
+        })
+    },
+    getLastFiveCategories: function (){
+        return client.getEntries({
+            'content_type': 'categorie',
+            limit: 5,
+            order: 'sys.createdAt'
+        })
+    }
+}
